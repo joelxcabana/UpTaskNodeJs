0302187ff4b272d5b695c6ade64c14fe65444a2e
joelCabana <joelxcabana@gmail.com>
2021-06-01 21:39:50 -0300

    creacion del controlador

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//controlador
+const proyectoController = require('../controllers/proyectosController');
+
 module.exports = function (){
-    
-        router.get('/',(req,res)=>{
-            res.json("HOME");
-        });
+        router.get('/',proyectoController.proyectosHome);
 
         router.get('/nosotros',(req,res)=>{
             res.send('NOSOTROS');

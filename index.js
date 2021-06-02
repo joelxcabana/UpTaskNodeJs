7a064b73caa6edc3fa41e1b68990720f1c05949e
joelCabana <joelxcabana@gmail.com>
2021-06-01 22:38:39 -0300

    habilitando los archivos estaticos y finalizando el master page

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 //crear aplicacion de express
 const app = express();
 
+//donde cargar los archivos estaticos
+app.use(express.static('public'));
+
 //habilitar pug
 app.set('view engine','pug');
 //agregar carpetas de las vistas

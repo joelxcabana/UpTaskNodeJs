da3ec9b1481b0739298d45d9448d401f62c752d4
joelCabana <joelxcabana@gmail.com>
2021-06-06 16:26:00 -0300

    agregar express validator para sanitizar al momento de insertar datos en la base de datos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//importar express valitador
+const { body } = require('express-validator/check'); 
+
 //controlador
 const proyectoController = require('../controllers/proyectosController');
 
@@ -9,7 +12,9 @@ module.exports = function (){
         router.get('/nuevo-proyecto',proyectoController.formularioPoryecto);
 
         //agregar nuevo proyecto
-        router.post('/nuevo-proyecto',proyectoController.nuevoProyecto);
+        router.post('/nuevo-proyecto',
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectoController.nuevoProyecto);
 
 
 return router;

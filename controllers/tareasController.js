f6f2b8f2e5a297079d9040d15d981a03b728405c
joelCabana <joelxcabana@gmail.com>
2021-06-09 22:45:11 -0300

    insertando tarea en la base de datos

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,5 +1,26 @@
 
-module.exports.agregarTarea = async (req,res) =>{
+const Proyectos = require('../models/Proyectos');
+const Tareas = require('../models/Tareas');
 
-res.send('enviado');
+module.exports.agregarTarea = async (req,res, next) =>{
+
+    //obtenemos el proyecto actual 
+    const url =  req.params.url;
+    const proyecto = await Proyectos.findOne({where : {url: url}});
+
+    const {tarea} = req.body;
+    //estado 0  = incompleto
+    const estado = 0;
+    const proyectoId = proyecto.id;
+
+    //insertar en la base de datos
+    
+    const resultado =  await Tareas.create({tarea,estado,proyectoId});
+
+    if(!resultado){
+        return next();
+    }
+ 
+    //redireccion
+    res.redirect(`/proyectos/${url}`);
 }

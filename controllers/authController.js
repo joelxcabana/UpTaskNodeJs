c294a925baaad0ffa077b9938ff6562c2dfde874
joelCabana <joelxcabana@gmail.com>
2021-06-20 20:37:37 -0300

    autenticar si esta logeado para acceder a rutas

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,13 @@ exports.autenticarUsuario =  passport.authenticate('local',{
    failureRedirect:'/iniciar-sesion',
    failureFlash:true
 });
+
+//funcion para revisar si el usuario esta logeado o no
+exports.usuarioAutenticado = (req,res,next) =>{
+   //si el usuario esta autenticado adelante
+   if(req.isAuthenticated()){
+      return next();
+   }
+   //sino redirigir al inicio
+    return res.redirect('/iniciar-sesion');
+}
--- routes/index.js
+++ routes/index.js
@@ -11,44 +11,84 @@ const usuariosController = require('../controllers/usuariosController');
 const authController = require('../controllers/authController');
 
 module.exports = function (){
-        router.get('/',proyectoController.proyectosHome);
-        router.get('/nuevo-proyecto',proyectoController.formularioPoryecto);
+
+        router.get('/',
+                authController.usuarioAutenticado,
+                proyectoController.proyectosHome
+        );
+
+        router.get('/nuevo-proyecto',
+                authController.usuarioAutenticado,
+                proyectoController.formularioPoryecto
+        );
 
         //agregar nuevo proyecto
         router.post('/nuevo-proyecto',
-        body('nombre').not().isEmpty().trim().escape(),
-        proyectoController.nuevoProyecto);
+                authController.usuarioAutenticado,
+                body('nombre').not().isEmpty().trim().escape(),
+                proyectoController.nuevoProyecto
+        );
 
         //listar proyecto
-        router.get('/proyectos/:url',proyectoController.proyectoPorUrl);
+        router.get('/proyectos/:url',
+                authController.usuarioAutenticado,
+                proyectoController.proyectoPorUrl
+        );
 
         //actualizar el proyecto
-        router.get('/proyectos/editar/:id',proyectoController.formularioEditar);
+        router.get('/proyectos/editar/:id',
+                authController.usuarioAutenticado,
+                proyectoController.formularioEditar
+        );
 
         //editar y guardar
         router.post('/nuevo-proyecto/:id',
-        body('nombre').not().isEmpty().trim().escape(),
-        proyectoController.actualizarProyecto);
+                authController.usuarioAutenticado,
+                body('nombre').not().isEmpty().trim().escape(),
+                proyectoController.actualizarProyecto
+        );
 
         //eliminar proyecto
-        router.delete('/proyectos/:url',proyectoController.eliminarProyecto)
+        router.delete('/proyectos/:url',
+                authController.usuarioAutenticado,
+                proyectoController.eliminarProyecto
+        )
 
         /* RUTAS PARA LAS TAREAS*/
-        router.post('/proyectos/:url',tareasController.agregarTarea);
+        router.post('/proyectos/:url',
+                authController.usuarioAutenticado,
+                tareasController.agregarTarea
+        );
 
         //actualizar tarea
-        router.patch('/tareas/:id',tareasController.cambiarEstadoTarea);
+        router.patch('/tareas/:id',
+                authController.usuarioAutenticado,
+                tareasController.cambiarEstadoTarea
+        );
 
         //eliminartarea
-        router.delete('/tareas/:id',tareasController.eliminarTarea);
+        router.delete('/tareas/:id',
+                authController.usuarioAutenticado,
+                tareasController.eliminarTarea
+        );
 
         //crear usuario
-        router.get('/crear-cuenta',usuariosController.formCrearCuenta);
-        router.post('/crear-cuenta',usuariosController.crearCuenta);
+        router.get('/crear-cuenta',
+                usuariosController.formCrearCuenta
+        );
+        
+        router.post('/crear-cuenta',
+                usuariosController.crearCuenta
+        );
 
         //iniciar sesion
-        router.get('/iniciar-sesion',usuariosController.formIniciarSesion);
+        router.get('/iniciar-sesion',
+                usuariosController.formIniciarSesion
+        );
 
-        router.post('/iniciar-sesion',authController.autenticarUsuario);
+        router.post('/iniciar-sesion',
+                authController.autenticarUsuario
+        );
+        
 return router;
 }
